4da4b2fb9fb3013682913b9a1e22c90527147e46
wavefunc <wavefunc@gmail.com>
2022-03-04 16:31:01 +0800

    button can move

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import './App.scss';
 import { Button } from 'react-bootstrap';
-import './App.scss'
+import { useState } from 'react';
 
 function App() {
+  const [condition, setCondition] = useState('__origin');
+
+  const moveButton = () => {
+    setCondition('__nextPhase');
+  };
+
   return (
-    <div>
-      <Button variant="primary" className="btn">Primary</Button>
+    <div className="main">
+      <Button variant="primary" className={`btn${condition}`} onClick={moveButton}>Button</Button>
     </div>
   );
 }
