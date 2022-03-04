d27eaa32af85cdb1b926850cdcd37f804e687e55
wavefunc <wavefunc@gmail.com>
2022-03-04 22:29:53 +0800

    start using react-animation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import './App.scss';
-import { Button } from 'react-bootstrap';
 import { useState } from 'react';
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import { bounce } from 'react-animations';
 
 function App() {
-  const [state, setState] = useState('__origin');
+  const [state, setState] = useState('');
+  var myRef = React.createRef();
+
+  const Bounce = styled.div`animation: 2s ${keyframes`${bounce}`} infinite`;
 
   const moveButton = () => {
-    setState('__nextPhase');
+    myRef.current.classList.toggle('__nextPhase')
+    // setState('__nextPhase');
+    console.log(myRef.current.classList);
   };
 
   return (
     <div className="main">
-      <Button variant="primary" className={`btn${state}`} onClick={moveButton}>Button</Button>
+      <Bounce>
+        <button
+          className="btn"
+          onClick={moveButton}
+          ref={myRef}
+        >button</button>
+      </Bounce>
     </div>
   );
 }
