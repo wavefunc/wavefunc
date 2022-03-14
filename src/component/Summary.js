e49347e94cf9607fb143af7a659422049cd9b888
wavefunc <wavefunc@gmail.com>
2022-03-14 16:38:06 +0800

    update

--- src/component/Summary.js
+++ src/component/Summary.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Container, Badge, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHtml5, faCss3Alt, faJsSquare, faBootstrap, faReact } from '@fortawesome/free-brands-svg-icons'
-
+import { faHtml5, faCss3Alt, faJsSquare, faBootstrap, faReact, faVuejs, faNodeJs, faPhp, faGitAlt, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 function Summary() {
    return (
@@ -36,7 +35,7 @@ function Summary() {
                   </div>
                </Row>
             </Container>
-         </div>         
+         </div>
          {/* 2 Front-End */}
          <div className='summary' >
             <Container className="p-3" fluid>
@@ -45,80 +44,89 @@ function Summary() {
                </Row>
                <Row>
                   <div>
-                     <Badge background-color="#fd7e14"><FontAwesomeIcon icon={faHtml5} />&nbsp;HTML</Badge> &nbsp;
-
+                     <Badge background-color="#fd7e14"><FontAwesomeIcon icon={faHtml5} />&nbsp;HTML</Badge>&nbsp;
                      <Badge><FontAwesomeIcon icon={faCss3Alt} />&nbsp;CSS</Badge>&nbsp;
                      <Badge><FontAwesomeIcon icon={faBootstrap} />&nbsp;Bootstrap</Badge>
                      <br />
                      <Badge bg="warning"><FontAwesomeIcon icon={faJsSquare} />&nbsp;JavaScript</Badge>&nbsp;
                      <Badge>&nbsp;jQuery</Badge>&nbsp;
-                     <Badge><FontAwesomeIcon icon={faReact} />&nbsp;React</Badge>
+                     <br />
+                     <Badge><FontAwesomeIcon icon={faReact} />&nbsp;React</Badge>&nbsp;
+                     <Badge><FontAwesomeIcon icon={faVuejs} />&nbsp;Vue.js</Badge>
                      <br />
                   </div>
                </Row>
             </Container>
-         </div>        
+         </div>
          {/* 3 Back-End */}
          <div className='summary' >
             <Container className="p-3" fluid>
                <Row>
-                  <h4>smry-id02</h4>
+                  <h4>Back-End</h4>
                </Row>
                <Row>
                   <div>
-                     擅於架構性思惟、對前端懷抱熱情<br />
+                     <Badge>&nbsp;MySQL</Badge>&nbsp;
+                     <Badge>&nbsp;Microsoft SQL Server</Badge>&nbsp;
+                     <br />
+                     <Badge><FontAwesomeIcon icon={faNodeJs} />&nbsp;Node.js</Badge>&nbsp;
+                     <Badge><FontAwesomeIcon icon={faPhp} />&nbsp;PHP</Badge>&nbsp;
                   </div>
                </Row>
             </Container>
-         </div>        
+         </div>
          {/* 4 Other skill */}
          <div className='summary' >
             <Container className="p-3" fluid>
                <Row>
-                  <h4>smry-id02</h4>
+                  <h4>Other skill</h4>
                </Row>
                <Row>
                   <div>
-                     擅於架構性思惟、對前端懷抱熱情<br />
+                     <Badge>&nbsp;axios</Badge>&nbsp;
+                     <Badge>&nbsp;RESTful API</Badge>&nbsp;
+                     <br />
+                     <Badge><FontAwesomeIcon icon={faGitAlt} />&nbsp;Git</Badge>&nbsp;
+                     <Badge><FontAwesomeIcon icon={faGithub} />&nbsp;Github</Badge>&nbsp;
                   </div>
                </Row>
             </Container>
-         </div>        
+         </div>
          {/* 5 作品集 Duckside */}
          <div className='summary' >
             <Container className="p-3" fluid>
                <Row>
-                  <h4>smry-id02</h4>
+                  <h4>作品集 - Duckside 投資管理網站</h4>
                </Row>
                <Row>
                   <div>
-                     擅於架構性思惟、對前端懷抱熱情<br />
+                     duckside<br />
                   </div>
                </Row>
             </Container>
-         </div>        
+         </div>
          {/* 6 作品集 bb減塑大進擊 */}
          <div className='summary' >
             <Container className="p-3" fluid>
                <Row>
-                  <h4>smry-id02</h4>
+                  <h4>作品集 - bb減塑大進擊</h4>
                </Row>
                <Row>
                   <div>
-                     擅於架構性思惟、對前端懷抱熱情<br />
+                     網址：<a href="https://wavefunc.github.io/bb-qa-game/">https://wavefunc.github.io/bb-qa-game/</a>
                   </div>
                </Row>
             </Container>
-         </div>        
+         </div>
          {/* 7 作品集 問候圖產生器 */}
          <div className='summary' >
             <Container className="p-3" fluid>
                <Row>
-                  <h4>smry-id02</h4>
+                  <h4>作品集 - 問候圖產生器</h4>
                </Row>
                <Row>
                   <div>
-                     擅於架構性思惟、對前端懷抱熱情<br />
+                  網址：<a href="https://wavefunc.github.io/greetPicMaker/">https://wavefunc.github.io/greetPicMaker/</a>
                   </div>
                </Row>
             </Container>
