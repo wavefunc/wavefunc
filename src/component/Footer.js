4c15e54d442dfbf5f3bf888e9b7953c05ec4e890
wavefunc <wavefunc@gmail.com>
2022-03-28 14:41:34 +0800

    update

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -8,17 +8,17 @@ function Footer() {
       <div id="footer" className='d-flex px-3 justify-content-between align-items-center'>
          <div className='d-inline-flex h-75 align-items-center'>
             <span className='mx-2 fs-6'>本站使用技術</span>
-            <Image className='mx-2' height="75%" src="/assets/webtech/html5.svg" />
-            <Image className='mx-2' height="75%" src="/assets/webtech/css3.svg" />
-            <Image className='mx-2' height="75%" src="/assets/webtech/javascript.svg" />
-            <Image className='mx-2' height="75%" src="/assets/webtech/react.png" />
-            <Image className='mx-2' height="75%" src="/assets/webtech/gsap.png" />
+            <Image className='mx-2' height="75%" src={`${process.env.PUBLIC_URL}/assets/webtech/html5.svg`} />
+            <Image className='mx-2' height="75%" src={`${process.env.PUBLIC_URL}/assets/webtech/css3.svg`} />
+            <Image className='mx-2' height="75%" src={`${process.env.PUBLIC_URL}/assets/webtech/javascript.svg`} />
+            <Image className='mx-2' height="75%" src={`${process.env.PUBLIC_URL}/assets/webtech/react.png`} />
+            <Image className='mx-2' height="75%" src={`${process.env.PUBLIC_URL}/assets/webtech/gsap.png`} />
          </div>
          <div className='d-inline-flex h-75 align-items-center'>
             <span className='mx-2 fs-6'>我的連繫方式</span>
-            <a href="mailto:wavefunc@gmail.com" target="_blank"><FontAwesomeIcon icon={faEnvelope} className='mx-2'/></a>
-            <a href="https://www.linkedin.com/in/wavefunc" target="_blank"><FontAwesomeIcon icon={faLinkedin} className='mx-2'/></a>
-            <a href="https://github.com/wavefunc" target="_blank"><FontAwesomeIcon icon={faGithub} className='mx-2'/></a>
+            <a href="mailto:wavefunc@gmail.com" target="_blank"><FontAwesomeIcon icon={faEnvelope} className='mx-2' /></a>
+            <a href="https://www.linkedin.com/in/wavefunc" target="_blank"><FontAwesomeIcon icon={faLinkedin} className='mx-2' /></a>
+            <a href="https://github.com/wavefunc" target="_blank"><FontAwesomeIcon icon={faGithub} className='mx-2' /></a>
          </div>
       </div>
    );
